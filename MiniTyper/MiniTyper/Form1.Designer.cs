15385c7b6bfa844b0025b92f97d5d239c789ae81
DanteGvns <Dante.a.g.hi@gmail.com>
2026-09-01 11:08:34 -0400

    app now does Username and Password

--- MiniTyper/MiniTyper/Form1.Designer.cs
+++ MiniTyper/MiniTyper/Form1.Designer.cs
@@ -3,7 +3,8 @@
     partial class Form1
     {
         private System.ComponentModel.IContainer components = null;
-        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.TextBox FirstTextBox;
+        private System.Windows.Forms.TextBox SecondTextBox;
         private System.Windows.Forms.Button buttonSubmit;
 
         protected override void Dispose(bool disposing)
@@ -19,23 +20,31 @@
 
         private void InitializeComponent()
         {
-            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.FirstTextBox = new System.Windows.Forms.TextBox();
+            this.SecondTextBox = new System.Windows.Forms.TextBox();
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBoxInput
+            // FirstTextBox
             // 
-            this.textBoxInput.Location = new System.Drawing.Point(12, 12);
-            this.textBoxInput.Name = "textBoxInput";
-            this.textBoxInput.Size = new System.Drawing.Size(360, 23);
-            this.textBoxInput.TabIndex = 0;
+            this.FirstTextBox.Location = new System.Drawing.Point(12, 12);
+            this.FirstTextBox.Name = "FirstTextBox";
+            this.FirstTextBox.Size = new System.Drawing.Size(360, 23);
+            this.FirstTextBox.TabIndex = 0;
+            // 
+            // SecondTextBox
+            // 
+            this.SecondTextBox.Location = new System.Drawing.Point(12, 41);
+            this.SecondTextBox.Name = "SecondTextBox";
+            this.SecondTextBox.Size = new System.Drawing.Size(360, 23);
+            this.SecondTextBox.TabIndex = 1;
             // 
             // buttonSubmit
             // 
-            this.buttonSubmit.Location = new System.Drawing.Point(12, 50);
+            this.buttonSubmit.Location = new System.Drawing.Point(12, 70);
             this.buttonSubmit.Name = "buttonSubmit";
             this.buttonSubmit.Size = new System.Drawing.Size(100, 30);
-            this.buttonSubmit.TabIndex = 1;
+            this.buttonSubmit.TabIndex = 2;
             this.buttonSubmit.Text = "Submit";
             this.buttonSubmit.UseVisualStyleBackColor = true;
             this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
@@ -46,7 +55,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(384, 101);
             this.Controls.Add(this.buttonSubmit);
-            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.FirstTextBox);
+            this.Controls.Add(this.SecondTextBox);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Mini Typer";
--- MiniTyper/MiniTyper/Form1.cs
+++ MiniTyper/MiniTyper/Form1.cs
@@ -13,18 +13,40 @@ namespace MiniTyper
 
         private async void buttonSubmit_Click(object sender, EventArgs e)
         {
-            string textToType = textBoxInput.Text;
-            if (string.IsNullOrWhiteSpace(textToType))
+            string firstInput = FirstTextBox.Text;
+            string secondInput = SecondTextBox.Text;
+            if (string.IsNullOrWhiteSpace(firstInput) && string.IsNullOrWhiteSpace(secondInput))
             {
                 MessageBox.Show("Please enter text to type.");
                 return;
+            } else if (!string.IsNullOrWhiteSpace(firstInput) && string.IsNullOrWhiteSpace(secondInput)){
+                // Wait 3 seconds so you can switch windows
+                await Task.Delay(3000);
+
+                // Send Only the first line to the active window
+                SendKeys.SendWait(firstInput);
+                return;
+
+            } else if (!string.IsNullOrWhiteSpace(firstInput) && !string.IsNullOrWhiteSpace(secondInput)){
+                // Wait 3 seconds so you can switch windows
+                await Task.Delay(3000);
+
+                // Send first and second line to the active window
+                SendKeys.SendWait(firstInput);
+                SendKeys.SendWait("{TAB}");
+                SendKeys.SendWait(secondInput);
+                return;
+
+            } else if (string.IsNullOrWhiteSpace(firstInput) && string.IsNullOrWhiteSpace(secondInput))
+            {
+                MessageBox.Show("Please enter a username.");
+                return;
+            } else
+            {
+                MessageBox.Show("Error: Conditons not met");
+                return;
             }
 
-            // Wait 5 seconds so you can switch windows
-            await Task.Delay(5000);
-
-            // Send keystrokes to the active window
-            SendKeys.SendWait(textToType);
         }
     }
 }
